d3ec45d6df47084a1790b85392cca6206b24785b
mdrifatshahriar1125 <rifatshahriar1125@gmail.com>
2025-05-29 21:11:00 +0600

    visited button toggle with changing style

--- src/component/county/country.jsx
+++ src/component/county/country.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import './country.css'
 
 export default function Country({country}){
-    console.log(country)
+    // console.log(country)
+    const [visited,setvisited]=useState(false);
+
+    const visitcountry=()=>{
+        visited? setvisited(false):setvisited(true);
+        // setvisited(!visited)
+    }
     
     return(
-        <div className="singlecountry">
-            <h3>country name : {country.name.common}</h3>
-            <h3>country flag </h3>
+        <div className={`singlecountry ${visited && "visited"}`}>
+            <h3>Name of the country : {country.name.common}</h3>
+            <h3>official name : {country.name.official}</h3>
+
+            
             <img src={country.flags.png} alt="flag not found yet" />
+            <button className={visited?'btn-visited':null} onClick={visitcountry}>{
+                visited?'visited':'not visited'
+                }</button>
+
         </div>
     )
 
